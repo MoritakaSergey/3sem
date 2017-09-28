fd7777d388e215035536c1f24a7931624d1e7344
MoritakaSergey <moritaka.s@mail.ru>
2017-09-29 00:18:51 +0300

    add something like constructor

--- task_2/dpipe.c
+++ task_2/dpipe.c
@@ -7,3 +7,22 @@ typedef struct {
 	int txd[2];
 	int rxd[2];	
 } dpipe_t;
+
+int dpipe(dpipe_t dp)
+{
+        int channel1[2];
+        int channel2[2];
+        if (pipe(channel1) == -1) {
+                exit(-1);
+        }
+        if (pipe(channel2) == -1) {
+                close(channel1[0]);
+                close(channel1[1]);
+                exit(-1);
+        }
+        dp.txd[0] = channel2[1];
+        dp.rxd[0] = channel1[0];
+        dp.txd[1] = channel1[1];
+        dp.rxd[1] = channel2[0];
+        return 0;
+}
