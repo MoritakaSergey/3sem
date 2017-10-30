32241b3631f605bde60cb0e073467fb0805751bd
MoritakaSergey <moritaka.s@mail.ru>
2017-10-30 21:36:30 +0300

    receiver: include libraries and definitions

---
+++ task_4/filecopying_receiver.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<sys/ipc.h>
+#include<sys/msg.h>
+#include<fcntl.h>
+#include<errno.h>
+#include<string.h>
+#include<unistd.h>
+
+#define ANYTYPE 0
+#define START 3
+#define DATA 2
+#define END 1
+
