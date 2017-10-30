05b8587a448405d899a275d93957f4ee6af36a10
MoritakaSergey <moritaka.s@mail.ru>
2017-10-30 21:46:54 +0300

    sender/receiver: add function which returns maximum capacity of a single message in bytes

--- task_4/filecopying_sender.c
+++ task_4/filecopying_sender.c
@@ -15,3 +15,14 @@
 #define DATA 2
 #define END 1
 
+int maxMessageSize(int msqid) {
+        int msgmax, code;
+        struct msqid_ds info;
+        if ((code = msgctl(msqid, MSG_INFO, &info)) < 0) {
+                 printf("ERROR: can\'t execute MSG_INFO command\n");
+                 exit(-1);
+        }
+        msgmax = ((struct msginfo*)(&info))->msgmax;
+        return msgmax;
+}
+
